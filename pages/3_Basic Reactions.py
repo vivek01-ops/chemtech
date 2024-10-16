d4bd49e9eff7072259b59402936a5fd356e75069
Vivek Chaudhari <165157025+vivek01-ops@users.noreply.github.com>
2024-10-16 19:40:56 +0530

    Update 3_Basic Reactions.py

--- pages/3_Basic Reactions.py
+++ pages/3_Basic Reactions.py
@@ -59,17 +59,17 @@ if st.button('Perform Reaction'):
         st.write(f"**Substrate:** {', '.join(substrate)}")
         
         with st.status('Performing the reaction...', expanded=True):
+            for seconds in range(40):
+                st.write(f"⏳ {seconds} seconds have passed")
+                time.sleep(1)
+            st.write(":material/check:Wait")
+            
             reaction_representation = (
             f"""
             **Reaction**:
             **Reactants:** {reactants[0]} + {substrate[0]} --(**Temp:** {temperature} K)--> 
             **Products:** 
             """.strip()
-
-            for seconds in range(40):
-                st.write(f"⏳ {seconds} seconds have passed")
-                time.sleep(1)
-            st.write(":material/check:")
         )
 
         # Initialize the generative model
