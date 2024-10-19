74e38dd61f763b34f0258353a740b4c9adb32818
Vivek Chaudhari <165157025+vivek01-ops@users.noreply.github.com>
2024-10-19 12:58:09 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Set page configuration
 st.set_page_config(
     page_title="Chemical Reaction Simulator",
-    page_icon="💊",
+    page_icon="🔬",
     layout="centered",
     initial_sidebar_state="collapsed"
 )
